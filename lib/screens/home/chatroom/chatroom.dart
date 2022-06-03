a45c0931867c996bb9e84d7ac2ea27cc1052896c
Alexander Dehn <alexander.dehn@nerdware.dev>
2022-06-03 12:27:00 +0200

    Remove prints

--- lib/screens/home/chatroom/chatroom.dart
+++ lib/screens/home/chatroom/chatroom.dart
@@ -1,4 +1,3 @@
-import 'package:acroworld/data.dart';
 import 'package:acroworld/graphql/model/community_messages/community_message.dart';
 import 'package:acroworld/graphql/model/community_messages/community_messages.dart';
 import 'package:acroworld/graphql/model/user/user.dart';
@@ -6,7 +5,6 @@ import 'package:acroworld/graphql/subscriptions.dart';
 import 'package:acroworld/models/message_model.dart';
 import 'package:acroworld/provider/user_provider.dart';
 import 'package:acroworld/screens/home/chatroom/app_bar_chatroom.dart';
-import 'package:acroworld/screens/home/chatroom/message_list.dart';
 import 'package:acroworld/screens/home/chatroom/message_text_field.dart';
 import 'package:acroworld/screens/home/chatroom/message_tile.dart';
 import 'package:acroworld/shared/constants.dart';
@@ -28,9 +26,6 @@ class Chatroom extends StatelessWidget {
     final UserProvider userProvider =
         Provider.of<UserProvider>(context, listen: false);
     final Map<String, dynamic>? parsedJwt = userProvider.parsedJwt;
-
-    print('userProvider.parsedJwt');
-    print(userProvider.parsedJwt);
 
     final String userId = parsedJwt!['sub'];
     return Scaffold(
@@ -71,8 +66,6 @@ class Chatroom extends StatelessWidget {
                         CommunityMessage? message = messages[index];
                         CommunityMessage? nextMessage = messages[index + 1];
                         CommunityMessage? previousMessage = messages[index - 1];
-                        print(index);
-                        print(message);
                         User? fromUser = message!.fromUser;
 
                         final isMe = userId == fromUser!.id;
