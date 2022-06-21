d0f9d4e22b1eea7356092f68cbd570c9aac2f36e
baumeister <baumeister@anyframe.com>
2022-06-21 17:45:31 +0200

    color jams

--- lib/screens/home/jam/jams/jam_tile.dart
+++ lib/screens/home/jam/jams/jam_tile.dart
@@ -1,7 +1,9 @@
 import 'package:acroworld/models/jam_model.dart';
+import 'package:acroworld/provider/user_provider.dart';
 import 'package:acroworld/screens/home/jam/jam_overview/jam_overview.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
 class JamTile extends StatelessWidget {
   const JamTile({required this.jam, required this.cid, Key? key})
@@ -12,6 +14,10 @@ class JamTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String uid = Provider.of<UserProvider>(context, listen: false).getId();
+    print("uid: $uid");
+    print(jam.participants);
+    bool paticipate = jam.participants.contains(uid);
     return GestureDetector(
       onTap: () => Navigator.of(context).push(
         MaterialPageRoute(
@@ -23,6 +29,7 @@ class JamTile extends StatelessWidget {
       child: Container(
         constraints: const BoxConstraints(maxWidth: 300.0),
         child: Card(
+          color: paticipate ? Colors.green : Colors.white,
           child: ListTile(
             title: Text(jam.name),
             subtitle: Text(DateFormat('kk:mm').format(jam.date)),
