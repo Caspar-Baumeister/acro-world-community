e143e428f45195ca3d9e03f9068cb71f668eb18a
baumeister <baumeister@anyframe.com>
2022-08-13 10:12:24 +0200

    fucking chat bubbles

--- lib/screens/home/chatroom/message_tile.dart
+++ lib/screens/home/chatroom/message_tile.dart
@@ -1,5 +1,4 @@
 import 'package:acroworld/models/community_messages/community_message.dart';
-import 'package:acroworld/shared/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -34,8 +33,6 @@ class MessageTile extends StatelessWidget {
             padding: const EdgeInsets.symmetric(vertical: 2.0, horizontal: 8.0),
             // space between whole messages
             margin: EdgeInsets.only(bottom: sameAuthorThenBevor ? 2.0 : 10.0),
-            constraints: BoxConstraints(
-                maxWidth: MediaQuery.of(context).size.width * 0.7),
             decoration: BoxDecoration(
               border: Border.all(
                 color: Colors.grey[200]!,
@@ -55,44 +52,51 @@ class MessageTile extends StatelessWidget {
     );
   }
 
-  Widget buildMessage(BuildContext context) => Column(
-        crossAxisAlignment:
-            isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-        children: <Widget>[
-          (isMe || sameAuthorThenNext)
-              ? const SizedBox(
-                  width: 0,
-                )
-              : Text(
-                  message.fromUser?.name ?? "",
+  Widget buildMessage(BuildContext context) => Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Column(
+            crossAxisAlignment:
+                isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+            children: <Widget>[
+              (isMe || sameAuthorThenNext)
+                  ? const SizedBox(
+                      width: 0,
+                    )
+                  : Text(
+                      message.fromUser?.name ?? "",
+                      style: const TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w800,
+                          fontSize: 16),
+                      //textAlign: isMe ? TextAlign.end : TextAlign.start,
+                    ),
+              Container(
+                alignment: isMe ? Alignment.centerRight : Alignment.centerLeft,
+                constraints: BoxConstraints(
+                    maxWidth: MediaQuery.of(context).size.width * 0.7),
+                child: Text(
+                  message.content ?? "",
+                  //textWidthBasis: TextWidthBasis.longestLine,
                   style: const TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.w800,
-                      fontSize: 16),
-                  //textAlign: isMe ? TextAlign.end : TextAlign.start,
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
+                  textAlign: TextAlign.start,
                 ),
-          Container(
-            color: SECONDARY_COLOR,
-            alignment: isMe ? Alignment.centerRight : Alignment.centerLeft,
-            child: Text(
-              message.content ?? "",
-              //textWidthBasis: TextWidthBasis.longestLine,
-              style: const TextStyle(
-                color: Colors.black,
-                fontSize: 16,
               ),
-              textAlign: TextAlign.start,
-            ),
+              message.createdAt != null
+                  ? Container(
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        DateFormat.Hm().format(
+                            DateTime.parse(message.createdAt!).toLocal()),
+                        style:
+                            const TextStyle(color: Colors.grey, fontSize: 12),
+                      ))
+                  : Container()
+            ],
           ),
-          message.createdAt != null
-              ? Container(
-                  alignment: Alignment.centerRight,
-                  child: Text(
-                    DateFormat.Hm()
-                        .format(DateTime.parse(message.createdAt!).toLocal()),
-                    style: const TextStyle(color: Colors.grey, fontSize: 12),
-                  ))
-              : Container()
         ],
       );
 }
