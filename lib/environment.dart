5892e047c30061affee74890387d91dca0544455
Alexander Dehn <alexander.dehn@nerdware.dev>
2022-09-10 12:54:36 +0200

    Use dev host for debug apps

---
+++ lib/environment.dart
@@ -0,0 +1,6 @@
+const bool isProduction = bool.fromEnvironment('dart.vm.product');
+
+class AppEnvironment {
+  static const String backendHost =
+      isProduction ? "bro-devs.com" : "dev.bro-devs.com";
+}
