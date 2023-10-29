11ac0f745bacaf75b9962e8ed59029dcb8151d31
baumeister <baumeister@anyframe.com>
2023-10-29 09:36:59 +0100

    add stripe payment

--- android/app/src/main/kotlin/com/example/acroworld/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/acroworld/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.acroworld
 
+import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+
+class MainActivity: FlutterFragmentActivity() {
 }
