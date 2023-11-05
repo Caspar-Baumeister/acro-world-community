0d0fa00bf246a2766b87fb4d16d9d3eb95176507
baumeister <baumeister@anyframe.com>
2023-11-05 22:45:31 +0100

    checkout modal new design

--- lib/screens/home_screens/activities/components/booking/booking_modal.dart
+++ lib/screens/home_screens/activities/components/booking/booking_modal.dart
@@ -1,151 +1,146 @@
-import 'package:acroworld/components/buttons/standart_button.dart';
 import 'package:acroworld/models/booking_option.dart';
-import 'package:acroworld/models/class_event.dart';
-import 'package:acroworld/models/class_model.dart';
-import 'package:acroworld/screens/home_screens/activities/components/booking/checkout_modal.dart';
 import 'package:acroworld/utils/colors.dart';
 import 'package:acroworld/utils/helper_functions/helper_functions.dart';
 import 'package:acroworld/utils/text_styles.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
-class BookingModal extends StatefulWidget {
-  const BookingModal(
-      {Key? key,
-      required this.classEvent,
-      required this.placesLeft,
-      required this.refetch})
-      : super(key: key);
+// class BookingModal extends StatefulWidget {
+//   const BookingModal(
+//       {Key? key,
+//       required this.classEvent,
+//       required this.placesLeft,
+//       required this.refetch})
+//       : super(key: key);
 
-  final ClassEvent classEvent;
-  final num placesLeft;
-  final void Function()? refetch;
+//   final ClassEvent classEvent;
+//   final num placesLeft;
+//   final void Function()? refetch;
 
-  @override
-  State<BookingModal> createState() => _BookingModalState();
-}
+//   @override
+//   State<BookingModal> createState() => _BookingModalState();
+// }
 
-class _BookingModalState extends State<BookingModal> {
-  late String? currentOption;
-  bool loading = false;
+// class _BookingModalState extends State<BookingModal> {
+//   late String? currentOption;
+//   bool loading = false;
 
-  @override
-  void initState() {
-    super.initState();
-    currentOption =
-        widget.classEvent.classModel?.classBookingOptions?[0].bookingOption?.id;
-  }
+//   @override
+//   void initState() {
+//     super.initState();
+//     currentOption =
+//         widget.classEvent.classModel?.classBookingOptions?[0].bookingOption?.id;
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    // UserProvider userProvider = Provider.of<UserProvider>(context);
-    double width = MediaQuery.of(context).size.width;
-    ClassModel clas = widget.classEvent.classModel!;
+//   @override
+//   Widget build(BuildContext context) {
+//     // UserProvider userProvider = Provider.of<UserProvider>(context);
+//     double width = MediaQuery.of(context).size.width;
+//     ClassModel clas = widget.classEvent.classModel!;
 
-    BookingOption? currentOptionObject;
-    if (currentOption != null) {
-      currentOptionObject = clas.classBookingOptions!
-          .firstWhere((e) => e.bookingOption?.id == currentOption)
-          .bookingOption!;
-    }
+//     BookingOption? currentOptionObject;
+//     if (currentOption != null) {
+//       currentOptionObject = clas.classBookingOptions!
+//           .firstWhere((e) => e.bookingOption?.id == currentOption)
+//           .bookingOption!;
+//     }
 
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(24.0, 5.0, 24.0, 24.0),
-      child: SafeArea(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Divider(
-              color: PRIMARY_COLOR,
-              thickness: 5.0,
-              indent: width * 0.40,
-              endIndent: width * 0.40,
-            ),
-            const SizedBox(height: 12.0),
-            Text(
-              "Booking options for ${clas.name} on ${DateFormat('EEEE, H:mm').format(widget.classEvent.date)}",
-              style: H16W7,
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 20.0),
-            Column(children: [
-              ...clas.classBookingOptions!
-                  .map((e) => Padding(
-                        padding: const EdgeInsets.all(8.0).copyWith(right: 20),
-                        child: BookOption(
-                          bookingOption: e.bookingOption!,
-                          value: currentOption == e.bookingOption!.id!,
-                          onChanged: (_) {
-                            setState(() {
-                              currentOption = e.bookingOption!.id!;
-                            });
-                          },
-                        ),
-                      ))
-                  .toList(),
-            ]),
-            // const SizedBox(height: 20),
-            // Text(
-            //   "A non-refundable deposit ${currentOptionObject != null ? ("of ${currentOptionObject.deposit().toStringAsFixed(2)}${getCurrecySymbol(currentOptionObject.currency)}") : ""} will be required to confirm your reservation. The remaining balance ${currentOptionObject != null ? ("of ${currentOptionObject.toPayOnArrival().toStringAsFixed(2)}${getCurrecySymbol(currentOptionObject.currency)}") : ""} will be charged upon your arrival or as determined by the establishment. Please note that the deposit amount cannot be refunded in case of cancellation.",
-            //   style: H12W4,
-            // ),
-            const SizedBox(height: 20),
-            StandartButton(
-              text: "Continue",
-              onPressed: () {
-                // close this modal and open the payment modal
-                Navigator.of(context).pop();
-                buildMortal(
-                    context,
-                    CheckoutModal(
-                      classEvent: widget.classEvent,
-                      bookingOption: currentOptionObject!,
-                    ));
-              },
-              //   onPressed: () async {
-              //     setState(() {
-              //       loading = true;
-              //     });
-              //     print(
-              //         "https://${AppEnvironment.backendHost}/api/payment/?bookingOptionId=${currentOptionObject!.id}&?classEventId=${widget.classEvent.id}&?userId=${userProvider.activeUser!.id}");
-              //     await Navigator.push(
-              //       context,
-              //       MaterialPageRoute(
-              //         builder: (context) => BookClassWebView(
-              //             initialUrl:
-              //                 "https://${AppEnvironment.backendHost}/api/payment/?bookingOptionId=${currentOptionObject!.id}&?classEventId=${widget.classEvent.id}&?userId=${userProvider.activeUser!.id}",
-              //             onFinish: () {
-              //               Fluttertoast.showToast(
-              //                   msg:
-              //                       "Order completed, we have send an email with your name to the astablishment",
-              //                   toastLength: Toast.LENGTH_SHORT,
-              //                   gravity: ToastGravity.TOP,
-              //                   timeInSecForIosWeb: 2,
-              //                   backgroundColor: Colors.green,
-              //                   textColor: Colors.white,
-              //                   fontSize: 16.0);
-              //               widget.refetch!();
-              //               Navigator.of(context).pop();
-              //             }),
-              //       ),
-              //     );
-              //     setState(() {
-              //       loading = false;
-              //     });
-              //   },
-              //   icon: Icons.paypal,
-              // ),
-            ),
-            Text(
-              "${widget.placesLeft} places left",
-              style: H10W4,
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
+//     return Padding(
+//       padding: const EdgeInsets.fromLTRB(24.0, 5.0, 24.0, 24.0),
+//       child: SafeArea(
+//         child: Column(
+//           mainAxisSize: MainAxisSize.min,
+//           children: [
+//             Divider(
+//               color: PRIMARY_COLOR,
+//               thickness: 5.0,
+//               indent: width * 0.40,
+//               endIndent: width * 0.40,
+//             ),
+//             const SizedBox(height: 12.0),
+//             Text(
+//               "Booking options for ${clas.name} on ${DateFormat('EEEE, H:mm').format(widget.classEvent.date)}",
+//               style: H16W7,
+//               textAlign: TextAlign.center,
+//             ),
+//             const SizedBox(height: 20.0),
+//             Column(children: [
+//               ...clas.classBookingOptions!
+//                   .map((e) => Padding(
+//                         padding: const EdgeInsets.all(8.0).copyWith(right: 20),
+//                         child: BookOption(
+//                           bookingOption: e.bookingOption!,
+//                           value: currentOption == e.bookingOption!.id!,
+//                           onChanged: (_) {
+//                             setState(() {
+//                               currentOption = e.bookingOption!.id!;
+//                             });
+//                           },
+//                         ),
+//                       ))
+//                   .toList(),
+//             ]),
+//             // const SizedBox(height: 20),
+//             // Text(
+//             //   "A non-refundable deposit ${currentOptionObject != null ? ("of ${currentOptionObject.deposit().toStringAsFixed(2)}${getCurrecySymbol(currentOptionObject.currency)}") : ""} will be required to confirm your reservation. The remaining balance ${currentOptionObject != null ? ("of ${currentOptionObject.toPayOnArrival().toStringAsFixed(2)}${getCurrecySymbol(currentOptionObject.currency)}") : ""} will be charged upon your arrival or as determined by the establishment. Please note that the deposit amount cannot be refunded in case of cancellation.",
+//             //   style: H12W4,
+//             // ),
+//             const SizedBox(height: 20),
+//             StandartButton(
+//               text: "Continue",
+//               onPressed: () {
+//                 // close this modal and open the payment modal
+//                 Navigator.of(context).pop();
+//                 buildMortal(
+//                     context,
+//                     CheckoutModal(
+//                       classEvent: widget.classEvent,
+//                       bookingOption: currentOptionObject!,
+//                     ));
+//               },
+//               //   onPressed: () async {
+//               //     setState(() {
+//               //       loading = true;
+//               //     });
+//               //     print(
+//               //         "https://${AppEnvironment.backendHost}/api/payment/?bookingOptionId=${currentOptionObject!.id}&?classEventId=${widget.classEvent.id}&?userId=${userProvider.activeUser!.id}");
+//               //     await Navigator.push(
+//               //       context,
+//               //       MaterialPageRoute(
+//               //         builder: (context) => BookClassWebView(
+//               //             initialUrl:
+//               //                 "https://${AppEnvironment.backendHost}/api/payment/?bookingOptionId=${currentOptionObject!.id}&?classEventId=${widget.classEvent.id}&?userId=${userProvider.activeUser!.id}",
+//               //             onFinish: () {
+//               //               Fluttertoast.showToast(
+//               //                   msg:
+//               //                       "Order completed, we have send an email with your name to the astablishment",
+//               //                   toastLength: Toast.LENGTH_SHORT,
+//               //                   gravity: ToastGravity.TOP,
+//               //                   timeInSecForIosWeb: 2,
+//               //                   backgroundColor: Colors.green,
+//               //                   textColor: Colors.white,
+//               //                   fontSize: 16.0);
+//               //               widget.refetch!();
+//               //               Navigator.of(context).pop();
+//               //             }),
+//               //       ),
+//               //     );
+//               //     setState(() {
+//               //       loading = false;
+//               //     });
+//               //   },
+//               //   icon: Icons.paypal,
+//               // ),
+//             ),
+//             Text(
+//               "${widget.placesLeft} places left",
+//               style: H10W4,
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 class BookOption extends StatelessWidget {
   const BookOption(
--- lib/screens/home_screens/activities/components/booking/checkout_modal.dart
+++ lib/screens/home_screens/activities/components/booking/checkout_modal.dart
@@ -1,197 +1,197 @@
-import 'dart:convert';
+// import 'dart:convert';
 
-import 'package:acroworld/components/buttons/custom_button.dart';
-import 'package:acroworld/models/booking_option.dart';
-import 'package:acroworld/models/class_event.dart';
-import 'package:acroworld/provider/user_provider.dart';
-import 'package:acroworld/utils/colors.dart';
-import 'package:acroworld/utils/text_styles.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_stripe/flutter_stripe.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:http/http.dart' as http;
-import 'package:intl/intl.dart';
-import 'package:provider/provider.dart';
+// import 'package:acroworld/components/buttons/custom_button.dart';
+// import 'package:acroworld/models/booking_option.dart';
+// import 'package:acroworld/models/class_event.dart';
+// import 'package:acroworld/provider/user_provider.dart';
+// import 'package:acroworld/utils/colors.dart';
+// import 'package:acroworld/utils/text_styles.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_stripe/flutter_stripe.dart';
+// import 'package:fluttertoast/fluttertoast.dart';
+// import 'package:http/http.dart' as http;
+// import 'package:intl/intl.dart';
+// import 'package:provider/provider.dart';
 
-// makes an test payment with stripe to the account acct_1O5td34FPJL5TYTc
+// // makes an test payment with stripe to the account acct_1O5td34FPJL5TYTc
 
-class CheckoutModal extends StatefulWidget {
-  const CheckoutModal(
-      {Key? key, required this.classEvent, required this.bookingOption})
-      : super(key: key);
+// class CheckoutModal extends StatefulWidget {
+//   const CheckoutModal(
+//       {Key? key, required this.classEvent, required this.bookingOption})
+//       : super(key: key);
 
-  // get the selected class event and the selected booking option
-  final ClassEvent classEvent;
-  final BookingOption bookingOption;
+//   // get the selected class event and the selected booking option
+//   final ClassEvent classEvent;
+//   final BookingOption bookingOption;
 
-  @override
-  State<CheckoutModal> createState() => _CheckoutModalState();
-}
+//   @override
+//   State<CheckoutModal> createState() => _CheckoutModalState();
+// }
 
-class _CheckoutModalState extends State<CheckoutModal> {
-  bool _ready = false;
+// class _CheckoutModalState extends State<CheckoutModal> {
+//   bool _ready = false;
 
-  @override
-  void initState() {
-    super.initState();
-    initPaymentSheet();
-  }
+//   @override
+//   void initState() {
+//     super.initState();
+//     initPaymentSheet();
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(24.0, 5.0, 24.0, 24.0),
-      child: SafeArea(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Divider(
-              color: PRIMARY_COLOR,
-              thickness: 5.0,
-              indent: width * 0.40,
-              endIndent: width * 0.40,
-            ),
-            const SizedBox(height: 12.0),
-            const Text(
-              "Checkout",
-              style: H16W7,
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 20.0),
-            // a column of 3 random booking options
-            Column(
-              children: [
-                // show the user the class event information
-                Text(
-                  "Class: ${widget.classEvent.classModel!.name}",
-                  style: H16W7,
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(height: 20.0),
-                Text(
-                  "Date: ${DateFormat('EEEE, H:mm').format(widget.classEvent.date)}",
-                  style: H16W7,
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(height: 20.0),
-                // a row with the booking option name and price
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Booking option: ${widget.bookingOption.title}",
-                      style: H16W7,
-                      textAlign: TextAlign.center,
-                    ),
-                    const SizedBox(width: 20.0),
-                    Text(
-                      "Price: ${widget.bookingOption.price}€",
-                      style: H16W7,
-                      textAlign: TextAlign.center,
-                    ),
-                  ],
-                ),
+//   @override
+//   Widget build(BuildContext context) {
+//     double width = MediaQuery.of(context).size.width;
+//     return Padding(
+//       padding: const EdgeInsets.fromLTRB(24.0, 5.0, 24.0, 24.0),
+//       child: SafeArea(
+//         child: Column(
+//           mainAxisSize: MainAxisSize.min,
+//           children: [
+//             Divider(
+//               color: PRIMARY_COLOR,
+//               thickness: 5.0,
+//               indent: width * 0.40,
+//               endIndent: width * 0.40,
+//             ),
+//             const SizedBox(height: 12.0),
+//             const Text(
+//               "Checkout",
+//               style: H16W7,
+//               textAlign: TextAlign.center,
+//             ),
+//             const SizedBox(height: 20.0),
+//             // a column of 3 random booking options
+//             Column(
+//               children: [
+//                 // show the user the class event information
+//                 Text(
+//                   "Class: ${widget.classEvent.classModel!.name}",
+//                   style: H16W7,
+//                   textAlign: TextAlign.center,
+//                 ),
+//                 const SizedBox(height: 20.0),
+//                 Text(
+//                   "Date: ${DateFormat('EEEE, H:mm').format(widget.classEvent.date)}",
+//                   style: H16W7,
+//                   textAlign: TextAlign.center,
+//                 ),
+//                 const SizedBox(height: 20.0),
+//                 // a row with the booking option name and price
+//                 Row(
+//                   mainAxisAlignment: MainAxisAlignment.center,
+//                   children: [
+//                     Text(
+//                       "Booking option: ${widget.bookingOption.title}",
+//                       style: H16W7,
+//                       textAlign: TextAlign.center,
+//                     ),
+//                     const SizedBox(width: 20.0),
+//                     Text(
+//                       "Price: ${widget.bookingOption.price}€",
+//                       style: H16W7,
+//                       textAlign: TextAlign.center,
+//                     ),
+//                   ],
+//                 ),
 
-                const SizedBox(height: 20.0),
-                // a cutsombutton that opens the stripe payment modal
-                CustomButton(
-                  "Pay now",
-                  () async {
-                    await Stripe.instance.presentPaymentSheet();
-                  },
-                  loading: !_ready,
-                )
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+//                 const SizedBox(height: 20.0),
+//                 // a cutsombutton that opens the stripe payment modal
+//                 CustomButton(
+//                   "Pay now",
+//                   () async {
+//                     await Stripe.instance.presentPaymentSheet();
+//                   },
+//                   loading: !_ready,
+//                 )
+//               ],
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
 
-  Future<void> initPaymentSheet() async {
-    try {
-      // 1. create payment intent on the server
-      final data = await _createTestPaymentSheet();
+//   Future<void> initPaymentSheet() async {
+//     try {
+//       // 1. create payment intent on the server
+//       final data = await _createTestPaymentSheet();
 
-      // define some billing details
-      const billingDetails = BillingDetails(
-        email: "ca@as.de",
-        phone: "+49123456789",
-        address: Address(
-          city: "Berlin",
-          country: "DE",
-          line1: "Test street 1",
-          line2: "Test street 2",
-          postalCode: "12345",
-          state: "Berlin",
-        ),
-      );
+//       // define some billing details
+//       const billingDetails = BillingDetails(
+//         email: "ca@as.de",
+//         phone: "+49123456789",
+//         address: Address(
+//           city: "Berlin",
+//           country: "DE",
+//           line1: "Test street 1",
+//           line2: "Test street 2",
+//           postalCode: "12345",
+//           state: "Berlin",
+//         ),
+//       );
 
-      // 2. initialize the payment sheet
-      final response = await Stripe.instance.initPaymentSheet(
-        paymentSheetParameters: SetupPaymentSheetParameters(
-          // Set to true for custom flow
-          customFlow: false,
-          // Main params
-          merchantDisplayName: 'Flutter Stripe Store Demo',
-          paymentIntentClientSecret: data['paymentIntent'],
-          // Customer keys
-          customerEphemeralKeySecret: data['ephemeralKey'],
-          customerId: data['customer'],
-          // Extra options
-          style: ThemeMode.dark,
-          billingDetails: billingDetails,
-        ),
-      );
-      print("Response from payment sheet: ${response.toString()}");
-      setState(() {
-        _ready = true;
-      });
-    } catch (e) {
-      // show flutter toast with error
-      Fluttertoast.showToast(
-          msg: "Error initializing payment sheet: ${e.toString()}",
-          toastLength: Toast.LENGTH_SHORT,
-          gravity: ToastGravity.TOP,
-          timeInSecForIosWeb: 2,
-          backgroundColor: Colors.red,
-          textColor: Colors.white,
-          fontSize: 16.0);
+//       // 2. initialize the payment sheet
+//       final response = await Stripe.instance.initPaymentSheet(
+//         paymentSheetParameters: SetupPaymentSheetParameters(
+//           // Set to true for custom flow
+//           customFlow: false,
+//           // Main params
+//           merchantDisplayName: 'Flutter Stripe Store Demo',
+//           paymentIntentClientSecret: data['paymentIntent'],
+//           // Customer keys
+//           customerEphemeralKeySecret: data['ephemeralKey'],
+//           customerId: data['customer'],
+//           // Extra options
+//           style: ThemeMode.dark,
+//           billingDetails: billingDetails,
+//         ),
+//       );
+//       print("Response from payment sheet: ${response.toString()}");
+//       setState(() {
+//         _ready = true;
+//       });
+//     } catch (e) {
+//       // show flutter toast with error
+//       Fluttertoast.showToast(
+//           msg: "Error initializing payment sheet: ${e.toString()}",
+//           toastLength: Toast.LENGTH_SHORT,
+//           gravity: ToastGravity.TOP,
+//           timeInSecForIosWeb: 2,
+//           backgroundColor: Colors.red,
+//           textColor: Colors.white,
+//           fontSize: 16.0);
 
-      rethrow;
-    }
-  }
+//       rethrow;
+//     }
+//   }
 
-  Future<Map<String, dynamic>> _createTestPaymentSheet() async {
-    // get User id
-    String? userId =
-        Provider.of<UserProvider>(context, listen: false).activeUser?.id;
-    if (userId == null) {
-      throw Exception("User id is null");
-    }
-    // if platform is android, use 10.0.2.2 instead of localhost
-    String host = "localhost"; // "10.0.2.2"; //
-    // if (Theme.of(context).platform == TargetPlatform.android) {
-    //   host = "10.0.2.2";
-    // }
-    // 1. create payment intent on the server (localhost for now)
-    final url = Uri.parse('http://$host:3000/stripe/create-payment-sheet');
-    // 2. create a body with {"amount" : 300, "destination": "acct_1O5td34FPJL5TYTc", "currency": "eur", "application_fee_amount": 10}
-    final body = jsonEncode({
-      "amount": 300,
-      "destination": "acct_1O5td34FPJL5TYTc",
-      "currency": "eur",
-      "application_fee_amount": 10,
-      "user_id": userId
-    });
-    // 3. make a post request to the url with the body
-    final response = await http.post(url,
-        headers: {'Content-Type': 'application/json'}, body: body);
-    // print the response with title
-    print("Response from server: ${response.body}");
+//   Future<Map<String, dynamic>> _createTestPaymentSheet() async {
+//     // get User id
+//     String? userId =
+//         Provider.of<UserProvider>(context, listen: false).activeUser?.id;
+//     if (userId == null) {
+//       throw Exception("User id is null");
+//     }
+//     // if platform is android, use 10.0.2.2 instead of localhost
+//     String host = "localhost"; // "10.0.2.2"; //
+//     // if (Theme.of(context).platform == TargetPlatform.android) {
+//     //   host = "10.0.2.2";
+//     // }
+//     // 1. create payment intent on the server (localhost for now)
+//     final url = Uri.parse('http://$host:3000/stripe/create-payment-sheet');
+//     // 2. create a body with {"amount" : 300, "destination": "acct_1O5td34FPJL5TYTc", "currency": "eur", "application_fee_amount": 10}
+//     final body = jsonEncode({
+//       "amount": 300,
+//       "destination": "acct_1O5td34FPJL5TYTc",
+//       "currency": "eur",
+//       "application_fee_amount": 10,
+//       "user_id": userId
+//     });
+//     // 3. make a post request to the url with the body
+//     final response = await http.post(url,
+//         headers: {'Content-Type': 'application/json'}, body: body);
+//     // print the response with title
+//     print("Response from server: ${response.body}");
 
-    return jsonDecode(response.body);
-  }
-}
+//     return jsonDecode(response.body);
+//   }
+// }
