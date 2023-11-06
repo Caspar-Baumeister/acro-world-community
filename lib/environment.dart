dd4559e72fbe2eeff771cc67df1dad3acfc4f260
Alexander Dehn <alexander.dehn@nerdware.dev>
2023-11-06 18:13:31 +0100

    Fix everything again

--- lib/environment.dart
+++ lib/environment.dart
@@ -1,10 +1,11 @@
 const bool isProduction = bool.fromEnvironment('dart.vm.product');
 
 class AppEnvironment {
-  static const bool isProdBuild = isProduction;
+  // static const bool isProdBuild = isProduction;
+  static const bool isProdBuild = true;
   static const String backendHost =
-      isProduction ? "bro-devs.com" : "dev.acroworld.de";
-  static const String dashboardUrl = isProduction
+      AppEnvironment.isProdBuild ? "bro-devs.com" : "dev.acroworld.de";
+  static const String dashboardUrl = AppEnvironment.isProdBuild
       ? "https://teacher.acroworld.de"
       : "https://admin-dev.acroworld.de";
 }
