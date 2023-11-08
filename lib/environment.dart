bbb9637549e14ecc646dd7a503c352fb78f6c6ce
baumeister <baumeister@anyframe.com>
2023-11-08 10:47:05 +0100

    fixed firebase 45

--- lib/environment.dart
+++ lib/environment.dart
@@ -1,10 +1,10 @@
 const bool isProduction = bool.fromEnvironment('dart.vm.product');
 
 class AppEnvironment {
-  static const bool isProdBuild = true; //isProduction;
+  static const bool isProdBuild = isProduction;
   static const String backendHost =
-      AppEnvironment.isProdBuild ? "bro-devs.com" : "dev.acroworld.de";
-  static const String dashboardUrl = AppEnvironment.isProdBuild
+      isProduction ? "bro-devs.com" : "dev.acroworld.de";
+  static const String dashboardUrl = isProduction
       ? "https://teacher.acroworld.de"
       : "https://admin-dev.acroworld.de";
 }
