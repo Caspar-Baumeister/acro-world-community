3fde36e4440b4ddb5e81f41c47c6384d5b48cb5b
baumeister <baumeister@anyframe.com>
2023-12-02 12:41:54 +0100

    ...

--- lib/screens/home_screens/activities/components/booking/booking_information_modal.dart
+++ lib/screens/home_screens/activities/components/booking/booking_information_modal.dart
@@ -4,7 +4,6 @@ import 'package:acroworld/models/class_event.dart';
 import 'package:acroworld/models/class_model.dart';
 import 'package:acroworld/screens/single_class_page/single_class_query_wrapper.dart';
 import 'package:acroworld/utils/colors.dart';
-import 'package:acroworld/utils/helper_functions/helper_functions.dart';
 import 'package:acroworld/utils/text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -22,10 +21,6 @@ class BookingInformationModal extends StatelessWidget {
     double width = MediaQuery.of(context).size.width;
     ClassModel clas = classEvent.classModel!;
 
-    num leftToPay = bookingOption.price! *
-        (100 - (bookingOption.commission! + bookingOption.discount!)) *
-        0.01;
-
     return Padding(
       padding: const EdgeInsets.fromLTRB(24.0, 5.0, 24.0, 24.0),
       child: SafeArea(
@@ -40,7 +35,7 @@ class BookingInformationModal extends StatelessWidget {
             ),
             const SizedBox(height: 12.0),
             Text(
-              "You have successfully reserved ${clas.name} on ${DateFormat('EEEE, H:mm').format(classEvent.startDateDT)}",
+              "You have successfully booked ${clas.name} on ${DateFormat('EEEE, H:mm').format(classEvent.startDateDT)}",
               style: H16W7,
               textAlign: TextAlign.center,
             ),
@@ -48,7 +43,7 @@ class BookingInformationModal extends StatelessWidget {
             Text(
               """Booking option: ${bookingOption.title}. 
 We have sent a confirmation email with your name to the organizer.
-You have to pay the remaining ${leftToPay.toStringAsFixed(2) + getCurrecySymbol(bookingOption.currency)} when you arrive.""",
+You can also view your booking details in your profile.""",
               style: H14W4,
             ),
             const SizedBox(height: 20),
