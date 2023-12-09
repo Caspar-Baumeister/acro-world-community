010a957a75a7c405209c2e46457b34121f7af080
Alexander Dehn <alexander.dehn@nerdware.dev>
2023-12-09 11:25:02 +0100

    Reset app.dart

--- lib/App.dart
+++ lib/App.dart
@@ -6,20 +6,17 @@ import 'package:acroworld/provider/activity_provider.dart';
 import 'package:acroworld/provider/event_filter_provider.dart';
 import 'package:acroworld/provider/place_provider.dart';
 import 'package:acroworld/provider/user_provider.dart';
-import 'package:acroworld/services/notification_service.dart';
 import 'package:acroworld/utils/theme.dart';
 import 'package:flutter/material.dart';
-import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:provider/provider.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 class App extends StatelessWidget {
   final ValueNotifier<GraphQLClient> client;
 
   const App({Key? key, required this.client}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    print("App:build");
     return MultiProvider(
         providers: [
           ChangeNotifierProvider(create: (_) => UserProvider()),
@@ -36,30 +33,5 @@ class App extends StatelessWidget {
             home: const ConnectionWrapper(),
           ),
         ));
-  }
-}
-
-class FirebaseMessengerWrapper extends StatefulWidget {
-  const FirebaseMessengerWrapper({Key? key}) : super(key: key);
-
-  @override
-  State<FirebaseMessengerWrapper> createState() =>
-      _FirebaseMessengerWrapperState();
-}
-
-class _FirebaseMessengerWrapperState extends State<FirebaseMessengerWrapper> {
-  // NotificationService notificationService = NotificationService();
-
-  @override
-  void initState() {
-    print("_FirebaseMessengerWrapperState:initState");
-    // notificationService.initialize(context);
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    print("_FirebaseMessengerWrapperState:build");
-    return const ConnectionWrapper();
   }
 }
