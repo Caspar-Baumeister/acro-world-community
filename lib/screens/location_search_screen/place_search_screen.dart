26f9c7bcb04b6349267eaf513d6970b53c2830af
baumeister <baumeister@anyframe.com>
2024-06-04 13:39:12 +0200

    bug fix

--- lib/screens/location_search_screen/place_search_screen.dart
+++ lib/screens/location_search_screen/place_search_screen.dart
@@ -1,4 +1,5 @@
 import 'package:acroworld/components/standard_app_bar/standard_app_bar.dart';
+import 'package:acroworld/screens/base_page.dart';
 import 'package:flutter/material.dart';
 
 import 'place_search_body.dart';
@@ -10,11 +11,11 @@ class PlaceSearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return const BasePage(
       appBar: StandardAppBar(
         title: 'Search a location',
       ),
-      body: PlaceSearchBody(),
+      child: PlaceSearchBody(),
     );
   }
 }
