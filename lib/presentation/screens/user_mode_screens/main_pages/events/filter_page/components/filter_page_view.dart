74b6b3a03ce92f020fc3c842456b4ef8162fe89c
baumeister <baumeister@anyframe.com>
2025-05-04 19:01:46 +0200

    fixed filter page

--- lib/presentation/screens/user_mode_screens/main_pages/events/filter_page/components/filter_page_view.dart
+++ lib/presentation/screens/user_mode_screens/main_pages/events/filter_page/components/filter_page_view.dart
@@ -1,6 +1,9 @@
+import 'package:acroworld/presentation/components/buttons/standart_button.dart';
 import 'package:acroworld/presentation/screens/user_mode_screens/main_pages/events/filter_page/components/filter_chip_cards.dart';
+import 'package:acroworld/provider/discover_provider.dart';
 import 'package:acroworld/utils/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class FilterPageView extends StatelessWidget {
   const FilterPageView({
@@ -9,6 +12,8 @@ class FilterPageView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    DiscoveryProvider discoveryProvider =
+        Provider.of<DiscoveryProvider>(context);
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: AppPaddings.medium),
       child: Column(
@@ -70,9 +75,41 @@ class FilterPageView extends StatelessWidget {
               ],
             ),
           ),
+          Padding(
+            padding: const EdgeInsets.all(AppPaddings.small),
+            child: Center(
+              child: Text(
+                "This filter will show ${discoveryProvider.filteredEventOccurencesLength.toString()} results",
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+            ),
+          ),
           const SizedBox(
             height: 20,
           ),
+          Center(
+            child: StandardButton(
+              text: "Continue",
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              isFilled: true,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Center(
+            child: StandardButton(
+              text: "Reset",
+              onPressed: () {
+                discoveryProvider.resetFilter();
+
+                Navigator.of(context).pop();
+              },
+            ),
+          ),
+          const SizedBox(height: 40),
         ],
       ),
     );
