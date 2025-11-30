625f6c56121fe5fe3b93082393bad21909ce10fa
Caspar Baumeister <caspar.baumeister@burmester.de>
2025-11-30 12:27:43 +0100

    little bug fix and editing build gradle

--- lib/utils/helper_functions/find_billing_teacher.dart
+++ lib/utils/helper_functions/find_billing_teacher.dart
@@ -1,6 +1,6 @@
 import 'package:acroworld/data/models/class_event.dart';
 
-ClassTeacher? findFirstTeacherOrNull(List<ClassTeacher>? classTeachers) {
+ClassTeachers? findFirstTeacherOrNull(List<ClassTeachers>? classTeachers) {
   for (var teacher in classTeachers ?? []) {
     if (teacher.teacher?.isStripeEnabled == true &&
         teacher.teacher?.stripeId != null) {
